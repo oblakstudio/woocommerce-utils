2fd0f81560e1ac4e7e5d63ed95320e6532125923
Sibin Grasic <sibin.grasic@oblak.studio>
2024-02-09 23:22:28 +0100

    fix: Maintenance and tweaks

--- src/Utils/woocommerce-utils-functions.php
+++ src/Utils/woocommerce-utils-functions.php
@@ -18,18 +18,18 @@ function wc_atds() {
     return WC_Data_Store::load( 'attribute_taxonomy' );
 }
 
-if ( ! function_exists( 'wc_deregister_all_blocks' ) && function_exists( 'wc_clean' ) ) :
+if ( ! function_exists( 'wc_deregister_all_blocks' ) ) :
     /**
      * Deregisters all WooCommerce blocks.
      */
 	function wc_deregister_all_blocks() {
-        //phpcs:ignore WordPress.Security.NonceVerification.Recommended
-        if ( is_admin() && str_starts_with( wc_clean( wp_unslash( $_GET['page'] ?? '' ) ), 'wc-' ) ) {
+        //phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+        if ( is_admin() && str_starts_with( wp_unslash( $_GET['page'] ?? '' ), 'wc-' ) ) {
             return;
         }
 
         if ( did_action( 'plugins_loaded' ) && ! doing_action( 'plugins_loaded' ) ) {
-            wc_doing_it_wrong(
+            _doing_it_wrong(
                 __FUNCTION__,
                 'This function needs to be called during the plugins_loaded action.',
                 '1.0.0',
@@ -37,7 +37,6 @@ if ( ! function_exists( 'wc_deregister_all_blocks' ) && function_exists( 'wc_cle
             return;
         }
 
-        wc_override_packages_class();
         wc_override_packages_class();
         add_action(
             'init',
